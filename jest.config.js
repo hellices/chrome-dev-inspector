534a016d0f9fa12be395c852db586ea1c8d9990f
hellices <hellices@naver.com>
2025-12-30 17:38:10 +0900

    Remove coverage threshold requirements

--- jest.config.js
+++ jest.config.js
@@ -1,14 +1,7 @@
 module.exports = {
   testEnvironment: 'jsdom',
   collectCoverageFrom: ['src/utils/**/*.js', '!src/**/*.test.js'],
-  coverageThreshold: {
-    global: {
-      branches: 80,
-      functions: 80,
-      lines: 80,
-      statements: 80,
-    },
-  },
+
   testMatch: ['**/tests/**/*.test.js'],
   transform: {
     '^.+\\.js$': ['babel-jest', { configFile: './babel.config.js' }],
